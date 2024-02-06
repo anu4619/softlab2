30951ba86be72cd6b4915c6d3b54934eab858610
anu4619 <125575070+anu4619@users.noreply.github.com>
2024-02-06 16:20:55 +0000

    used flask templete

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello_world2():
+    return 'Hello,World!'
+
+@app.route("/name")
+def hello_world1():
+    return 'Anushka!'
+
+@app.route("/age")
+def hello_world3():
+    return '19'
